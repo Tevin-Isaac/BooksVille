c789baf3fa5c89600d1299018953f7f6c3df70cf
Farzeen Kistnareddy <90398313+FarzeenKist@users.noreply.github.com>
2022-10-21 21:48:54 +0400

    fixes and cleanup

--- src/contracts/book_contract.py
+++ src/contracts/book_contract.py
@@ -9,9 +9,7 @@ class Book:
         price = Bytes("PRICE")
         sold = Bytes("SOLD")
         likes = Bytes("LIKES")  # Uint64
-        dislikes = Bytes("DISLIKES")  # Uint64
-        address = Bytes("ADDRESS")
-        owner = Bytes("OWNER")
+        liked = Bytes("liked")
 
     class AppMethods:
         like = Bytes("like")
@@ -20,27 +18,42 @@ class Book:
 
     def application_creation(self):
         return Seq([
+            # checks for any empty/invalid values in the input data
             Assert(Txn.application_args.length() == Int(4)),
             Assert(Txn.note() == Bytes("books:uv1")),
-            Assert(Btoi(Txn.application_args[3]) > Int(0)),
+            Assert(
+                And(
+                    Len(Txn.application_args[0])  > Int(0),
+                    Len(Txn.application_args[1])  > Int(0),
+                    Len(Txn.application_args[2])  > Int(0),
+                    Btoi(Txn.application_args[3]) > Int(0),
+                )
+            ),
             App.globalPut(self.Variables.name, Txn.application_args[0]),
             App.globalPut(self.Variables.image, Txn.application_args[1]),
             App.globalPut(self.Variables.description, Txn.application_args[2]),
             App.globalPut(self.Variables.price, Btoi(Txn.application_args[3])),
             App.globalPut(self.Variables.sold, Int(0)),
             App.globalPut(self.Variables.likes, Int(0)),
-            App.globalPut(self.Variables.dislikes, Int(0)),
-            App.globalPut(self.Variables.address, Global.creator_address()),
-            App.globalPut(self.Variables.owner, Txn.sender()),
             Approve()
         ])
 
+    def setOptIn(self):
+        return Seq([
+            App.localPut(Txn.sender(), self.Variables.liked, Int(0)),
+            Approve()  
+        ])
+
+    # allow users to buy a book
+    # checks are carried to ensure that the sender is a valid buyer and 
+    # the amount sent in the payment transactions matches the book price
     def buy(self):
         count = Txn.application_args[1]
         valid_number_of_transactions = Global.group_size() == Int(2)
 
         valid_payment_to_seller = And(
             Gtxn[1].type_enum() == TxnType.Payment,
+            Gtxn[0].sender() != Global.creator_address(),
             Gtxn[1].receiver() == Global.creator_address(),
             Gtxn[1].amount() == App.globalGet(self.Variables.price) * Btoi(count),
             Gtxn[1].sender() == Gtxn[0].sender(),
@@ -56,49 +69,23 @@ class Book:
 
         return If(can_buy).Then(update_state).Else(Reject())
 
-    # like
+    # likes a book
     def like(self):
 
-        Assert(
-            And(
-                    # The number of transactions within the group transaction must be exactly 2.
-                    # first one being the adopt function and the second being the payment transactions
-                    Global.group_size() == Int(1),
-
-                    # Txn.applications[0] is a special index denoting the current app being interacted with
-                    Txn.applications.length() == Int(1),
-
-                    # The number of arguments attached to the transaction should be exactly 2.
-                    Txn.application_args.length() == Int(1),
-
-            ),
-        )
-
         return Seq([
+            Assert(App.localGet(Txn.sender(), self.Variables.liked) == Int(0)),
+            App.localPut(Txn.sender(), self.Variables.liked, Int(1)),
             App.globalPut(self.Variables.likes, App.globalGet(self.Variables.likes) + Int(1)),
             Approve()
         ])
 
 
-    # dislike
+    # dislikes a book
     def dislike(self):
-
-        Assert(
-            And(
-                    # The number of transactions within the group transaction must be exactly 2.
-                    # first one being the adopt function and the second being the payment transactions
-                    Global.group_size() == Int(1),
-
-                    # Txn.applications[0] is a special index denoting the current app being interacted with
-                    Txn.applications.length() == Int(1),
-
-                    # The number of arguments attached to the transaction should be exactly 2.
-                    Txn.application_args.length() == Int(1),
-            ),
-        )
-
         return Seq([
-            App.globalPut(self.Variables.dislikes, App.globalGet(self.Variables.dislikes) + Int(1)),
+            Assert(App.localGet(Txn.sender(), self.Variables.liked) == Int(1)),
+            App.localPut(Txn.sender(), self.Variables.liked, Int(0)),
+            App.globalPut(self.Variables.likes, App.globalGet(self.Variables.likes) - Int(1)),
             Approve()
         ])
         
@@ -109,6 +96,7 @@ class Book:
         return Cond(
             [Txn.application_id() == Int(0), self.application_creation()],
             [Txn.on_completion() == OnComplete.DeleteApplication, self.application_deletion()],
+            [Txn.on_completion() == OnComplete.OptIn, self.setOptIn()],
             [Txn.application_args[0] == self.AppMethods.buy, self.buy()],
             [Txn.application_args[0] == self.AppMethods.like, self.like()],
             [Txn.application_args[0] == self.AppMethods.dislike, self.dislike()]
